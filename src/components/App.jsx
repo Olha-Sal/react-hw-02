36669c1b260a47092c6605029aeb953025ec4772
Olha-Sal <kosyuk.o@gmail.com>
2024-02-24 17:21:19 +0200

    refactoring the 'Feedback Widget' task code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,88 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './Feedback/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Message } from './Message/Message';
 
-export class App extends Component {
-  // Оголошуємо стан застосунку з трьома властивостями: good, neutral і bad
-  state = {
+const App = () => {
+  const [state, setState] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
   // Функція для збільшення кількості відгуків відповідної категорії
-  handleFeedback = e => {
-    console.log(e);
-    this.setState(prevState => ({
+  const handleFeedback = e => {
+    setState(prevState => ({
       ...prevState,
       [e]: prevState[e] + 1,
     }));
   };
 
   // Функція для підрахунку відсотка позитивних відгуків
-  countTotalFeedback = () => {
-    // console.log(this.state.good + this.state.neutral + this.state.bad);
-    return this.state.good + this.state.neutral + this.state.bad;
+  const countTotalFeedback = () => {
+    console.log(state.good + state.neutral + state.bad);
+    return state.good + state.neutral + state.bad;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
     if (total === 0) {
       return 0;
     }
-    return Math.round((this.state.good / total) * 100);
+    return Math.round((state.good / total) * 100);
   };
+  // Оголошуємо масив опцій для кнопок відгуків
+  const options = ['good', 'neutral', 'bad'];
 
-  // Функція для відображення кількості відгуків кожної категорії
-  // renderFeedback = () => {
-  //   return (
-  //     <div>
-  //       <h2>Statistics:</h2>
-  //       <p>Good: {this.state.good}</p>
-  //       <p>Neutral: {this.state.neutral}</p>
-  //       <p>Bad: {this.state.bad}</p>
-  //       <p>Total: {this.countTotalFeedback()}</p>
-  //       <p>Positive feedback: {this.countPositiveFeedbackPercentage()}%</p>
-  //     </div>
-  //   );
-  // };
-
-  render() {
-    // Оголошуємо масив опцій для кнопок відгуків
-    const options = ['good', 'neutral', 'bad'];
-
-    return (
-      <div className="App">
-        <Section title="Pleace leave your feedback">
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.handleFeedback}
+  return (
+    <div className="App">
+      <Section title="Pleace leave your feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={handleFeedback} />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() > 0 ? (
+          <Statistics
+            good={state.good}
+            neutral={state.neutral}
+            bad={state.bad}
+            getTotal={countTotalFeedback()}
+            getPositivePercentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
-        <Section title="Statistics">
-          {this.countTotalFeedback() > 0 ? (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              getTotal={this.countTotalFeedback}
-              getPositivePercentage={this.countPositiveFeedbackPercentage}
-            />
-          ) : (
-            <Message message="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
-    //   <div className="App">
-    //     <h1>Pleace leave feedback</h1>
-    //     <button onClick={() => this.handleFeedback('good')}>Good</button>
-    //     <button onClick={() => this.handleFeedback('neutral')}>Neutral</button>
-    //     <button onClick={() => this.handleFeedback('bad')}>Bad</button>
-    //     {this.renderFeedback()}
-    //   </div>
-    // );
-  }
-}
+        ) : (
+          <Message message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
+};
+
+export default App;
